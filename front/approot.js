d0d0acb92e331c25d02ad8a8a5af20cf241ab150
syr <cutieu-rim@naver.com>
2025-05-15 14:14:39 +0900

    알림창 모달

--- front/approot.js
+++ front/approot.js
@@ -1,15 +1,188 @@
 (function() {
   const { createElement, Fragment } = React;
   const { createRoot } = ReactDOM;
-
+  
   function openAlert() {
-    window.open('alert_pop.html', 'Alert', 'width=500,height=800');
-    // const modalBtn = document.querySelector(".modal-Btn");
-    // const modal = document.querySelector(".modal");
+    const modal = document.querySelector(".modal1");
+    
+    if (!modal) {
+      const div = document.createElement("div");
+      div.innerHTML =  `<dialog class="modal1" style="overflow-y:scroll;">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>프로젝트 1에 업무가 추가되었습니다.</div>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>QWER님께서 친구를 요청합니다.</div>
+                                <button style="margin-left:10px;" class="button_ale">수락</button>
+                                <button style="margin-left:5px;" class="button_ale">거부</button>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td> 
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>프로젝트 2에 초대되었습니다.</div>
+                                <div style="margin-left:10px;">(초대 유저: asdf12)</div>
+                                <button style="margin-left:10px;" class="button_ale">수락</button>
+                                <button style="margin-left:5px;" class="button_ale">거부</button>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td> 
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>프로젝트 1에 업무가 추가되었습니다.</div>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>프로젝트 2에 업무가 추가되었습니다.</div>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>스크롤 확인용1</div>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>스크롤 확인용1</div>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>스크롤 확인용1</div>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="display: flex;">
+                                <div>스크롤 확인용1</div>
+                            </div>
+                            <br/>
+                            <div style="display: flex;">
+                                <div style="font-size: small;">2025/05/05 12:00</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <img src="icon_root/trash.png" style="height: 20px; width: 20px; margin-top:55px;"/>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <form method="dialog">
+                <button>Close</button>
+            </form>
+        </dialog>`;
+        document.body.appendChild(div);
+    }
+    
+    //window.open('alert_pop.html', 'Alert', 'width=500,height=800');
+    const modalBtn = document.querySelector("#alert");
+    
 
-    // modalBtn.addEventListener("click", () => {
-    //     modal.showModal();
-    // })
+    modalBtn.addEventListener("click", () => {
+        modal.showModal();
+    }
+  );
+   
   }
   function openFriend() {
     window.open('friend_pop.html', 'Friend', 'width=800,height=800');
@@ -46,13 +219,23 @@
     
     createElement(
       'button',
-      { className: 'right1 drag1 clear', onClick: openAlert },
-      createElement('i', { className: 'fa-solid fa-bell fa-2x', style: { fontSize: '25px'} })
+      { className: 'right1 drag1 clear', onClick: openAlert, id:'alert' },
+      createElement('i', { className: 'fa-solid fa-bell fa-2x', style: { fontSize: '25px'}})
     )
   );
 
 }
   createRoot(document.getElementById('root')).render(createElement(approot));
+
+
+  window.onload = function() {openAlert();};
+
+  let target = document.querySelector("#root");
+  let observer = new MutationObserver((mutations) => {
+    openAlert();
+});
+  let option = { attributes: true, childList: true, characterData: true };
+  observer.observe(target, option);
 })();
 
 /*(function() {
@@ -61,7 +244,7 @@
 
   // function openAlert() {
   //   //window.open('alert_pop.html', 'Alert', 'width=500,height=800');
-  //   // const modal = document.querySelector('dialog.modal1');
+  //   // const modal = document.querySelector('.dialog.modal1');
   //   // if (modal) {
   //   //   modal.showModal();
   //   // } 
